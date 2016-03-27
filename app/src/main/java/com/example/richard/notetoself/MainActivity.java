69823772621943694549fc3dc39b44c366659255
richardbowmaker <rjb.accounts@live.co.uk>
2016-03-27 16:18:22 +0100

    added new note dialog and updated main activity

--- app/src/main/java/com/example/richard/notetoself/MainActivity.java
+++ app/src/main/java/com/example/richard/notetoself/MainActivity.java
@@ -8,9 +8,11 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+    Note mTempNote = new Note();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,29 +22,38 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View view)
+            public void onClick(View v)
             {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                DialogShowNote dialog = new DialogShowNote();
+                dialog.sendNoteSelected(mTempNote);
+                dialog.show(getFragmentManager(), "123");
             }
         });
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
+    public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
+    public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -54,6 +65,18 @@ public class MainActivity extends AppCompatActivity
             return true;
         }
 
+//        if (id == R.id.action_add)
+//        {
+//            DialogNewNote dialog = new DialogNewNote();
+//            dialog.show(getFragmentManager(), "");
+//            return true;
+//        }
+
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createNewNote(Note n)
+    {
+        mTempNote = n;
     }
 }
